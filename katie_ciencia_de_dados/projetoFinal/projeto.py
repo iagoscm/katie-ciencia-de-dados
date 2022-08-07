aad51a115a04ff53d93d056deadf11489069e1bf
iagoscm <iagomatosousa@gmail.com>
2022-08-07 16:13:37 -0300

    termina projeto
    Co-authored-by: alexianaa <alexiasilvacardosoo@gmail.com>

--- katie_ciencia_de_dados/projetoFinal/projeto.py
+++ katie_ciencia_de_dados/projetoFinal/projeto.py
@@ -10,7 +10,26 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import io
 
+# Le o conteudo do arquivo
 df = pd.read_csv('Students_data.csv')
+
+# Tratamento de dados (retirando o desnecessario)
 media = df.drop(columns=['class','GPA','ID','race','from1','from2','from3','from4','gender','y'])
+
+# Isola os aprovados
 aprovados = df['y']
-print(aprovados)
+
+# Faz a media dos alunos
+num = media.median('columns', True)
+
+# Criando e ordenando a tabela
+aprovados_table = {'aprovacoes': aprovados}
+aprovados_table = pd.DataFrame(data = aprovados_table)
+aprovados_table = aprovados_table.sort_values(by='aprovacoes')
+
+# Plotando o grafico
+plt.scatter(num, aprovados_table)
+plt.title("Media por tipo de aprovacao")
+plt.xlabel("medias")
+plt.ylabel("aprovacoes")
+plt.show()
